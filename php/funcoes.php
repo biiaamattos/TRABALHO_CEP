ad4855bf0ca0fcf51e5481d98ee2aecb2289e0d1
Beatriz Mattos <beatrizmattos290@gmail.com>
2022-09-05 09:02:09 -0300

    Trabalho de Porgramação Web

---
+++ php/funcoes.php
@@ -0,0 +1,13 @@
+<?php
+function buscarCEP($cep){
+    $cep = str_replace("-", "", $cep);
+
+    @$url = file_get_contents("http://viacep.com.br/ws/$cep/json/");
+
+    if (strlen($url) > 0) {
+        return $url;
+    } else {
+        return "";
+    }
+}
+echo buscarCEP($_POST["cep"]);
